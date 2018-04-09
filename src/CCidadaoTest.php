92abd6523f653ac3ad1fb16a3d96e5c7830c8a11
Daniel Silva <danny@dannyps.net>
2018-04-09 22:29:13 +0100

    Upgrade to PHPUnit v7

--- src/CCidadaoTest.php
+++ src/CCidadaoTest.php
@@ -1,22 +1,11 @@
 <?php
 
+require 'CCidadao.php';
 
-include 'CCidadao.php';
+use PHPUnit\Framework\TestCase;
 
-/**
- * CCidadao test case.
- */
-class CCidadaoTest extends PHPUnit_Framework_TestCase
+class CCidadaoTest extends TestCase
 {
-
-
-    public function testTest(){
-        $this->assertTrue(1 > 0);
-    }
-    public function testFail(){
-        $this->assertFalse(1 < 0);
-    }
-
     public function testCCD(){
         $this->assertEquals(8, CCidadao::getCCD(1569448));
         $this->assertEquals(1, CCidadao::getCCD(19283745));
@@ -27,4 +16,3 @@ class CCidadaoTest extends PHPUnit_Framework_TestCase
     }
 
 }
-
